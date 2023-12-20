29156114d494df6fa50a01a37b49d189cf44eb67
unknown <amv.harsh@gmail.com>
2023-12-21 00:42:44 +0530

    removed the database link

--- backend/dataBase/db.js
+++ backend/dataBase/db.js
@@ -1,6 +1,8 @@
 const mongoose=require('mongoose');
 
-const url='mongodb://127.0.0.1:27017/assignment'
+// enter your database url 
+
+const url="url"
 mongoose.connect(url).then((msg)=>{
     console.log("connected to database")
 }).catch((err)=>{
